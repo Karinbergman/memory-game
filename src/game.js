0e54808dbf81aa3430c0bab56892644724aba528
Karin Bergman <karin.bergman@milou.se>
2017-09-26 12:02:49 +0200

    Changed game to a component

--- src/game.js
+++ src/game.js
@@ -2,16 +2,38 @@ import React from "react"
 import Card from "./card"
 import Counter from "./counter"
 
-const Game = () => (
-  <div className="game">
-    <h1>Let{`'`}s <span>play</span> Memory <span>Beach</span> game<span>!</span></h1>
-    <div className="card-wrapper">
-      <Card src="/images/surfboard1.jpg" />
-      <Card src="/images/surfboard2.jpg" />
-      <Card src="/images/surfboard3.jpg" />
-      <Card src="/images/surfboard4.jpg" />
-    </div>
-  </div>
-)
+class Game extends React.Component {
+
+  state = {
+    cards: [
+      {
+        src: '/images/surfboard1.jpg'
+      },
+      {
+        src: '/images/surfboard2.jpg'
+      },
+      {
+        src: '/images/surfboard3.jpg'
+      },
+      {
+        src: '/images/surfboard4.jpg'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <h1>Let{`'`}s <span>play</span> Memory <span>Beach</span> game<span>!</span></h1>
+        <div className="card-wrapper">
+          <Card src={this.state.cards[0].src} />
+          <Card src={this.state.cards[1].src} />
+          <Card src={this.state.cards[2].src} />
+          <Card src={this.state.cards[3].src} />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Game
