b97db9c4a0c844505d947cee10747a4ff8584e7f
Karin Bergman <karin.bergman@milou.se>
2017-09-26 11:05:18 +0200

    Updated structure, css and background photo

--- src/game.js
+++ src/game.js
@@ -3,11 +3,13 @@ import Card from "./card"
 
 const Game = () => (
   <div className="game">
-    <h1>Memory game</h1>
-    <Card src="/images/surfboard1.jpg" />
-    <Card src="/images/surfboard2.jpg" />
-    <Card src="/images/surfboard3.jpg" />
-    <Card src="/images/surfboard4.jpg" />
+    <h1>Let{`'`}s <span>play</span> Memory <span>Beach</span> game<span>!</span></h1>
+    <div className="card-wrapper">
+      <Card src="/images/surfboard1.jpg" />
+      <Card src="/images/surfboard2.jpg" />
+      <Card src="/images/surfboard3.jpg" />
+      <Card src="/images/surfboard4.jpg" />
+    </div>
   </div>
 )
 
