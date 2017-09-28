72db88cdc700e96a334a9bd3f30b3d1fdfca0f29
Karin Bergman <karin.bergman@milou.se>
2017-09-28 16:29:59 +0200

    Added checked if cards match, close all cards and cards that are not matched

--- src/game.js
+++ src/game.js
@@ -36,17 +36,53 @@ class Game extends React.Component {
 
 // New function which will take one argument, a "card id" and logged it
   handledCardClicked = (cardId) => {
-    console.log(cardId);
     const newCardsState = this.state.cards.map((card) => {
-      if (card.id === cardId) {
-        card.isFlipped = !card.isFlipped;
+      if (card.id === cardId && !card.isFlipped) {
+        card.isFlipped = true;
       }
       return card;
     })
     this.setState ({
       cards: newCardsState
-    })
+    }, this.checkIfCardsMatched())
   }
+
+checkIfCardsMatched = () => {
+  const flippedCards = this.state.cards.filter((image) => {
+    return image.isFlipped;
+  })
+
+  if (flippedCards.length === 2) {
+    if (flippedCards[0].src === flippedCards[1].src) {
+      flippedCards[0].isMatched = true;
+      flippedCards[1].isMatched = true;
+    }
+    setTimeout(this.closeAllCards, 1000);
+  }
+}
+
+closeAllCards = () => {
+  const newCardsArray = this.state.cards.map((image) => {
+    image.isFlipped = false;
+    return image;
+  })
+  this.setState ({
+    cards: newCardsArray
+  })
+  if (this.cardsThatAreNotMatched()) {
+    alert('HEJ');
+  }
+}
+
+cardsThatAreNotMatched = () => {
+  const matchedNotCards = this.state.cards.filter((image) => {
+    return !image.isMatched;
+  })
+  if (matchedNotCards.length === 0) {
+    alert('YOU WON');
+  }
+  console.log(matchedNotCards);
+}
 
 // Create a new instance of the Card component with the following props:
 //  src: url of the photo
@@ -60,17 +96,32 @@ class Game extends React.Component {
       key={card.id}
       id={card.id}
       onCardClick={this.handledCardClicked}
-      isFlipped={card.isFlipped} />
+      isFlipped={card.isFlipped}
+      isMatched={card.isMatched} />
   }
 
   render() {
     return (
       <div className="game">
-        <h1>Let{`'`}s <span>play</span> Memory!</h1>
-        <h2>Try to find surfboards that matches</h2>
-        <div className="card-wrapper">
-          {this.state.cards.map(this.renderCard)}
-        </div>
+      {
+        (matchedNotCards.length === 0)
+          ? <h1>Let{`'`}s <span>play</span> Memory!</h1>
+            <h2>Try to find surfboards that matches</h2>
+            <div className="card-wrapper">
+              {this.state.cards.map(this.renderCard)}
+            </div>
+          : <div>
+            <h1>YOU WON!</h1> 
+              <button onClick={this.resetGame}>
+                Play again?
+              </button>
+            </div>
+      }
+            <h1>Let{`'`}s <span>play</span> Memory!</h1>
+            <h2>Try to find surfboards that matches</h2>
+            <div className="card-wrapper">
+              {this.state.cards.map(this.renderCard)}
+            </div>
       </div>
     )
   }
